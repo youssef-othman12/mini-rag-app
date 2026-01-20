3e12e78a66f4251e7b6b763cad276e13d5f6f525
youssef othman <yussufothman537@gmail.com>
2026-01-20 21:56:00 +0200

    On branch feat-004-(Uploading-a-File)  Your branch is up to date with 'origin/feat-004-(Uploading-a-File)'.
     Changes to be committed:
    	modified:   src/routes/base.py
    	new file:   src/routes/data.py

--- src/routes/base.py
+++ src/routes/base.py
@@ -1,13 +1,13 @@
-from fastapi import FastAPI , APIRouter
-from helpers.config import get_settings
+from fastapi import FastAPI , APIRouter , Depends
+from helpers.config import get_settings ,settings
 import os 
 base_router = APIRouter(
     prefix="/api/v1",
     tags =["api_v1"]
 )
 @base_router.get("/")
-async def welcome():
-    app_settings = get_settings()
+async def welcome(app_settings : settings = Depends(get_settings)):
+    
     app_name = app_setting.APP_NAME
     app_version = app_setting.APP_VERSION
     return{
