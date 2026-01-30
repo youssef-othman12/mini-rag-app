905e2f4c882500ba6094b801b88b5d5c918d3089
youssef othman <yussufothman537@gmail.com>
2026-01-30 02:21:13 +0200

    On branch feat-004-(Uploading-a-File)  Your branch is up to date with 'origin/feat-004-(Uploading-a-File)'.  Changes to be committed: 	modified:   src/assets/.gitignore 	modified:   src/controllers/DataController.py 	modified:   src/routes/data.py

--- src/controllers/DataController.py
+++ src/controllers/DataController.py
@@ -16,6 +16,7 @@ class DataController(BaseController):
         
         return True ,ResponseSignal.FILE_VALIDATED_SUCCESS.value
     def generate_unique_filename(self, orig_file_name:str,project_id):
+        
         random_key  = self.generate_random_string()
         project_path = ProjectController().get_project_path(project_id = project_id)
         
@@ -34,7 +35,7 @@ class DataController(BaseController):
             project_path,
             random_key + "_" + cleaned_file_name
             )
-            return new_file_path
+        return new_file_path
         
     def get_clean_file_name(self,orig_file_name:str) :
         # remove any special  chars except _ and .
--- src/routes/data.py
+++ src/routes/data.py
@@ -5,6 +5,9 @@ import os
 from controllers import DataController , ProjectController
 import aiofiles
 from models import ResponseSignal
+import logging
+
+logger = logging.getLogger('uvicorn.error')
 
 data_router = APIRouter(
     prefix="/api/v1/data",
@@ -29,9 +32,21 @@ async def upload_data(project_id : str ,file : UploadFile,
           orig_file_name=file.filename,
           project_id=project_id
       )
-      async with aiofiles.open(file_path,"wb") as f:
-          while chunk := await file.read(app_settings.FILE_DEFAULT_CHUNK_SIZE):
-              await f.write(chunk)
+      try:
+            async with aiofiles.open(file_path,"wb") as f:
+               while chunk := await file.read(app_settings.FILE_DEFAULT_CHUNK_SIZE):
+                await f.write(chunk)
+                
+      except Exception as e:
+          
+          logger.error(f"Error while uploading file: {e}")
+          
+          return JSONResponse(
+              status_code = status.HTTP_400_BAD_REQUEST,
+              content = {
+                  "signal":ResponseSignal.FILE_UPLOAD_FAILED.value
+              }
+          )
               
               
       return JSONResponse(
