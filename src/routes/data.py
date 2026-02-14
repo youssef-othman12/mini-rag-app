66b9c7dad5f35678bf098d49244e92a979f6acdc
youssef othman <yussufothman537@gmail.com>
2026-02-15 01:07:50 +0200

    On branch feat-005-(File-Processing)  Your branch is up to date with 'origin/feat-005-(File-Processing)'.  Changes to be committed: 	modified:   src/controllers/ProcessController.py 	modified:   src/controllers/__init__.py 	modified:   src/models/enums/ResponseEnums.py 	modified:   src/routes/data.py

--- src/routes/data.py
+++ src/routes/data.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI , APIRouter , Depends ,UploadFile, status
 from fastapi.responses import JSONResponse
 from helpers.config import get_settings ,settings
 import os 
-from controllers import DataController , ProjectController
+from controllers import DataController , ProjectController , ProcessController
 import aiofiles
 from models import ResponseSignal
 import logging
@@ -61,5 +61,26 @@ async def upload_data(project_id : str ,file : UploadFile,
 async def proccess_endpoint(project_id: str ,proccessrequest: proccessrequest ):
     
     file_id = proccessrequest.file_id
+    chunk_size = proccessrequest.chunk_size
+    overlap_size =proccessrequest.overlap_size
     
-    return file_id
+    process_controller = ProcessController(project_id=project_id)
+    
+    file_content = process_controller.get_file_content(file_id=file_id)
+    
+    file_chunks = process_controller.process_file_content(
+        file_content=file_content,
+        file_id=file_id,
+        chunk_size=chunk_size,
+        overlap_size=overlap_size
+    )
+    
+    if file_chunks == None or len(file_chunks)==0:
+        return JSONResponse(
+              status_code = status.HTTP_400_BAD_REQUEST,
+              content = {
+                  "signal":ResponseSignal.PROCESSING_FAILED.value
+              }
+          )
+        
+    return file_chunks
