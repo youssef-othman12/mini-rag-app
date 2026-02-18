d068cad2175fefae562faf167e4c9e1d7195b973
youssef othman <yussufothman537@gmail.com>
2026-02-18 15:25:15 +0200

    On branch feat-006  Your branch is up to date with 'origin/feat-006'.  Changes to be committed: 	modified:   src/.env.example 	modified:   src/helpers/config.py 	modified:   src/main.py 	modified:   src/requirements.txt

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -9,6 +9,9 @@ class settings(BaseSettings):
     FILE_ALLOWED_TYPES : list
     FILE_MAX_SIZE : int
     FILE_DEFAULT_CHUNK_SIZE : int
+    
+    MONGODB_URL: str
+    MONGODB_DATABASE: str
     model_config = SettingsConfigDict(env_file=".env")
     class config:
         env_file=".env"
